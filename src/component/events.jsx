d8807c7bb0f2ae35246f66d02115e9846bdcc6f2
Senadhian2003 <senadhian@gmail.com>
2022-10-26 16:37:18 +0530

    sena 3rd commit

--- src/component/events.jsx
+++ src/component/events.jsx
@@ -4,47 +4,66 @@ import Navigation from "./navbar";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 
-import image from "../image/dummy.png";
+import image from "../image/tech1.jpg";
 
 export default function Events() {
   return (
     <div>
       <Navigation />
-      <div>
-        <h2 className="eh1"> Technical Events </h2>
+      <h2 className="eh1"> Technical Events </h2>
 
-        <div className="container-fluid">
-          <div className="row ">
-            <div className="row ">
-              <img src={image} className="image" alt="" />
-            </div>
+      <div class="container c1 ">
+                <div class="row ">
+                  <div class="col-sm-6 col-md-4 col-lg-4 circle-1  ">
+                    <img src={image} alt="" />
+                  </div>
+                  <div class="s1 col-sm-6 col-md-4 col-lg-4">
+                    <div class="grid grid2">
+                
+                    </div>
+                  </div>
+                  <div class="s1 col-sm-6 col-md-4 col-lg-4">
+                    <div class="grid grid3">
+                
+                    </div>
+                  </div>
+                  
+                  
+              
+                    </div>
 
-            <div className="row">
-              <div className="col-6 para"></div>
-              <div className="col-6 ">
+                    <div className="row justify-content-center">
 
-              <div class="card" style={{width : '576px',
-                marginLeft : 'auto',
-                marginRight : 'auto'
-            }}>
-  <div class="card-body">
-    <h5 class="card-title" style={{textAlign : 'center'}} className="ctxt">PYTHON CODING</h5>
-   
-    <p class="card-text" style={{color : 'white'}}>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-   
-  </div>
-</div>
+                
+
+                
+                    <div class="s1 col-sm-6 col-md-4 col-lg-4 ">
+                      <div class="grid grid5 ">
+                  
+                      </div>
+                    </div>
+                    <div class="s1 col-sm-6 col-md-4 col-lg-4">
+                      <div class="grid grid6">
+                  
+                      </div>
+                    </div>
+                    
+                    
+                
+
+                    
+  
+                </div>
 
               </div>
-            </div>
 
-           
-          </div>
-        </div>
-      </div>
+              <div class="circle-1  ">
+              <img src="https://media-cdn.tripadvisor.com/media/photo-s/02/b7/c2/0b/skanderbeg-square.jpg" alt="Skanderbeg Square Albania" />
+                  </div>
+                
+
     </div>
   );
 }
