ed5e78da3f184083ae094f5ffefe672c67048b1c
Senadhian2003 <senadhian@gmail.com>
2022-10-26 17:49:20 +0530

    sena 4th commit

--- src/component/events.jsx
+++ src/component/events.jsx
@@ -5,19 +5,22 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
-
-import image from "../image/tech1.jpg";
+import { Particle } from './Particle';
+import image from "../image/dummy.png";
 
 export default function Events() {
   return (
     <div>
+        <Particle/>
       <Navigation />
       <h2 className="eh1"> Technical Events </h2>
 
       <div class="container c1 ">
-                <div class="row ">
-                  <div class="col-sm-6 col-md-4 col-lg-4 circle-1  ">
-                    <img src={image} alt="" />
+                <div class="row justify-content-center">
+                  <div class="s1 col-sm-6 col-md-4 col-lg-4 ">
+                    <div class="grid grid1">
+                        
+                    </div>
                   </div>
                   <div class="s1 col-sm-6 col-md-4 col-lg-4">
                     <div class="grid grid2">
@@ -58,12 +61,6 @@ export default function Events() {
                 </div>
 
               </div>
-
-              <div class="circle-1  ">
-              <img src="https://media-cdn.tripadvisor.com/media/photo-s/02/b7/c2/0b/skanderbeg-square.jpg" alt="Skanderbeg Square Albania" />
-                  </div>
-                
-
     </div>
   );
 }
