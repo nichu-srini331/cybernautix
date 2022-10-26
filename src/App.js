6dad184a8b850033111df3f6f3d1eb05ecf1dfaf
Senthamil2003 <77163996+Senthamil2003@users.noreply.github.com>
2022-10-26 19:16:09 +0530

    sentha commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Particle } from './component/Particle';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Events from './component/events';
+import Contact from './component/Contact';
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/events" element={<Events />} />
+                    <Route path="/contactus" element={<Contact/>} />
                 </Routes>
             </Router>
             <Particle/>
