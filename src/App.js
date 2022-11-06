9cf5394b191ac5e2bdcda02c8c17d98081898ea5
Maaz Hussain <maazhussain43@gmail.com>
2022-11-06 12:18:41 +0530

    Minute Changes

--- src/App.js
+++ src/App.js
@@ -22,23 +22,11 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/events" element={<Events />} />
                     <Route path="/events/:id" element={<EventDetails />} />
-                    {/* <Route path="/events-dFlag" element={<EventDetails data="Capture the Flag" />} />
-                    <Route path="/events-dBlind" element={<EventDetails data="Blind Coding" />} />
-                    <Route path="/events-dUI" element={<EventDetails data="UI Designing" />} />
-                    <Route path="/events-dSolo" element={<EventDetails data="Solo Coding" />} />
-                    <Route path="/events-dPaper" element={<EventDetails data="Paper Presentation" />} />
-                    <Route path="/events-dPoetry" element={<EventDetails data="Poetry Writing" />} />
-                    <Route path="/events-dConnx" element={<EventDetails data="Connextions" />} />
-                    <Route path="/events-dHunt" element={<EventDetails data="Treasure Hunt" />} />
-                    <Route path="/events-dCub" element={<EventDetails data="Cubix Solving" />} />
-                    <Route path="/events-dTal" element={<EventDetails data="Talent Gig" />} /> */}
                     <Route path="/contactus" element={<Contact/>} />
                 </Routes>
             </Router>
      </>
-    
     </div>
-    
   );
 }
 
